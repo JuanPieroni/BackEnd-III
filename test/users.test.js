09112fb30790c54436e0c716bc09c273a8bb1a5b
Juan Pablo Pieroni <seisduro@gmail.com>
2025-12-27 18:59:25 -0300

    test get user

--- test/users.test.js
+++ test/users.test.js
@@ -1,6 +1,6 @@
 import Users from "../src/dao/Users.dao.js"
 import { expect } from "chai"
-import { connectDB, disconnectDB } from './db.js'
+import { connectDB, disconnectDB } from "./db.js"
 
 before(async function () {
     await connectDB()
@@ -10,22 +10,71 @@ after(async function () {
     await disconnectDB()
 })
 
-/* describe("Create Users", function () {
-    it()
-  
-}) */
-
 describe("Obtener usuarios", function () {
+    before(function () {
+        this.usersDao = new Users()
+    })
+
+    beforeEach(async function () {
+        this.result = await this.usersDao.get()
+    })
+
     it("debe entregar un array de usuarios ", async function () {
-        const usersDao = new Users()
-        const result = await usersDao.get()
-        expect(result).to.be.an("array")
+        expect(this.result).to.be.an("array")
     })
 
     it("debe entregar un array con 1+ usuarios", async function () {
-        const usersDao = new Users()
-        const result = await usersDao.get()
-        expect(result).to.be.an("array")
-        expect(result.length).to.be.greaterThan(0)
+        expect(this.result).to.be.an("array")
+        expect(this.result.length).to.be.greaterThan(0)
     })
+
+    it("cada usuario debe tener las propiedades requeridas", async function () {
+        this.result.forEach((user) => {
+            expect(user).to.have.property("_id")
+            expect(user).to.have.property("first_name")
+            expect(user).to.have.property("last_name")
+            expect(user).to.have.property("email")
+            expect(user).to.have.property("password")
+            expect(user).to.have.property("role")
+            expect(user).to.have.property("pets")
+        })
+    })
+
+    it("cada usuario debe tener un email válido", async function () {
+        this.result.forEach((user) => {
+            expect(user.email).to.include("@")
+        })
+    })
+
+    it("cada usuario debe tener un array de pets", async function () {
+        this.result.forEach((user) => {
+            expect(user.pets).to.be.an("array")
+        })
+    })
+
+    it("el id debe ser un i dde mongo", async function () {
+        this.result.forEach((user) => {
+            expect(user._id.toString()).to.have.lengthOf(24)
+        })
+    })
+    it("el role debe ser 'user' o 'admin'", async function () {
+        this.result.forEach((user) => {
+            expect(user.role).to.be.oneOf(["user", "admin"])
+        })
+    })
+    it("el email debe tener formato válido", async function () {
+        this.result.forEach((user) => {
+            expect(user.email).to.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)
+        })
+    })
+    it("el password debe estar hasheado", async function () {
+        this.result.forEach((user) => {
+            expect(user.password).to.not.equal("coder123")
+            expect(user.password).to.have.lengthOf.greaterThan(20)
+        })
+    })
+})
+
+describe("Crear Usuario", function () {
+ 
 })
